9aef6b3c1c4fdeb62b7c68ca45a21086a6dfc5da
Joseph Farghal <jaf200000@utdallas.edu>
2024-02-12 00:53:22 -0600

    Removed MAP_ANONYMOUS

--- my-count.c
+++ my-count.c
@@ -48,7 +48,7 @@ void worker(int begin, int end, int id, int n, int m, int* Barrier, int* X)
     for (int i = 0; i < ceil(log2(n)); i++) 
     {
         int* Old = X;
-        X = mmap(NULL, n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
+        X = mmap(NULL, n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, -1 , 0);
         //shared->X = malloc(n * sizeof(int));
         for (int j = begin; begin < end; j++) 
         {
@@ -102,12 +102,12 @@ int main(int argc, char* argv[]) {
     //SharedData* shared = mmap(NULL, sizeof(SharedData), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
 
     //Shared memory initialization
-    int* shared_n = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
-    int* shared_m = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
-    int* A = mmap(NULL, n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
-    int* B = mmap(NULL, n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
-    int* X = mmap(NULL, n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1 , 0);
-    int* Barrier = mmap(NULL, m*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS , -1 , 0);
+    int* shared_n = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, -1 , 0);
+    int* shared_m = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, -1 , 0);
+    int* A = mmap(NULL, n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED , -1 , 0);
+    int* B = mmap(NULL, n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED , -1 , 0);
+    int* X = mmap(NULL, n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED , -1 , 0);
+    int* Barrier = mmap(NULL, m*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED , -1 , 0);
 
     //int* processId = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, inputFilePtr , 0);
 
